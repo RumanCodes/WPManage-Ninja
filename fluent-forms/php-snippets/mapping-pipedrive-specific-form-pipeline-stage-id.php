976a0f684329619cf1706d9e745a3c511ee44381
Ruman Ahmed <algorithmsunlocks@gmail.com>
2026-06-02 11:14:32 +0600

    Update mapping-pipedrive-specific-form-pipeline-stage-id.php

--- fluent-forms/php-snippets/mapping-pipedrive-specific-form-pipeline-stage-id.php
+++ fluent-forms/php-snippets/mapping-pipedrive-specific-form-pipeline-stage-id.php
@@ -17,3 +17,8 @@ add_filter('fluentform/integration_data_pipedrive', function ($postData, $feed, 
       }
       return $postData;
   }, 10, 3);
+
+  // To find your pipeline and stage IDs, you can call the Pipedrive API directly:
+//
+// Pipelines: https://api.pipedrive.com/v1/pipelines?api_token=YOUR_TOKEN
+// Stages: https://api.pipedrive.com/v1/stages?api_token=YOUR_TOKEN&pipeline_id=1
