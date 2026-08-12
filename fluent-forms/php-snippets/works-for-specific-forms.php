97d0d250de873d04b783b2c78182663558d67571
Ruman Ahmed <algorithmsunlocks@gmail.com>
2026-08-12 15:24:53 +0600

    new-updates
    new-updates

---
+++ fluent-forms/php-snippets/works-for-specific-forms.php
@@ -0,0 +1,19 @@
+<?php
+  add_filter('fluentform/insert_response_data', function ($formData, $formId, $inputConfigs) {
+      if ((int) $formId !== 128) {
+          return $formData;
+      }
+      foreach ($inputConfigs as $fieldName => $field) {
+          if (($field['element'] ?? '') !== 'textarea') {
+              continue;
+          }
+          if (empty($formData[$fieldName]) || !is_string($formData[$fieldName])) {
+              continue;
+          }
+          $value = str_replace(["\r\n", "\r"], "\n", $formData[$fieldName]);
+          $value = preg_replace('/[ \t]+$/m', '', $value);
+          $value = preg_replace("/\n{2,}/", "\n", $value);
+          $formData[$fieldName] = trim($value);
+      }
+      return $formData;
+  }, 10, 3);
